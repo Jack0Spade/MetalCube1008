83c4c4880d53033d697ae7cb4b1f3734fa983f06
Jack <49828053+Jack0Spade@users.noreply.github.com>
2020-07-15 19:53:24 +0800

    v1.1
    added media query for desktop

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,24 @@
+//  Mobile / Desktop detection
+
+let mobile = window.matchMedia('(max-width: 640px)');
+
+
+// Image swap on desktop
+
+
+if(mobile.matches){
+
+}else{
+    let coffee = document.querySelector('.coffee');
+    let table = document.querySelector('.table');
+
+    coffee.setAttribute('src', 'asset/coffee-desktop.png');
+    table.setAttribute('src', 'asset/background-table-desktop.png');
+}
+
+
 // Hamburger Script
+
 
 let burger = document.querySelector('.burger');
 
@@ -6,17 +26,38 @@ burger.addEventListener('click', function (){
     burger.classList.toggle('activated');
 });
 
+
 // Parallax Scrolling script
 
-let table = document.querySelector('.table');
-let text = document.querySelector('.intro');
-let coffee = document.querySelector('.coffee');
 
 window.addEventListener('scroll',function(){
+
+    let table = document.querySelector('.table');
+    let text = document.querySelector('.intro');
+    let coffee = document.querySelector('.coffee');
+
     let value = window.scrollY;
 
-    table.style.top = -value * 0.3 + 'px';
-    text.style.top = value * 0.1 + 'px';
-    coffee.style.top = -value * 0.4 + 'px';
+    if(mobile.matches){
+        table.style.top = -value * 0.3 + 'px';
+        text.style.top = value * 0.1 + 'px';
+        coffee.style.top = -value * 0.4 + 'px';
+    }else{
+        table.style.top = -value * 0.5 + 'px';
+        text.style.top = value * 0.3 + 'px';
+        coffee.style.top = -value * 0.6 + 'px';
+    }
+
 });
+
+
+// Navigation bar shrink script
+window.addEventListener('scroll', function(){
+
+    let navbar = document.querySelector('.navigation');
+
+    navbar.classList.toggle('shrink', window.scrollY > 100 && !mobile.matches);
+
+});
+
 
